af6db49593f4b73f2627dccdecd27675595608ff
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-04 17:33:12 -0500

    Update IOMainLayout.tsx

--- client/src/components/innerOrbit/layout/IOmainLayout/IOMainLayout.tsx
+++ client/src/components/innerOrbit/layout/IOmainLayout/IOMainLayout.tsx
@@ -1,16 +1,16 @@
-// file: c;ient/src/components/innerOrbit/layout/IOmainLayout/IOMainLayout.tsx
+// File: client/src/components/innerOrbit/layout/IOmainLayout/IOMainLayout.tsx
 
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 // Components
-import StarBackground from '../common/StarBackground';
-import IOMainLayoutBackground from '../../../components/innerOrbit/layout/IOmainLayout/IOMainLayoutBackground';
-import NavBar from '../layout/nav/IONavBar';
-import Footer from '../layout/nav/IOFooter';
+import StarBackground from '../../../common/StarBackground';
+import IOMainLayoutBackground from './IOMainLayoutBackground';
+import NavBar from '../nav/IONavBar';
+import Footer from '../nav/IOFooter';
 
 // Styles
-import styles from '../../../../../assets/css/innerOrbit/layout/IOMainLayoutBackground.module.css'; 
+import styles from '../../../../../assets/css/innerOrbit/layout/IOMainLayoutBackground.module.css';
 
 const IOMainLayout: React.FC = () => {
   return (
