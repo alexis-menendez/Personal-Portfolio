c0a9aac410684406180c2d46a621cd5389ba104b
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-04 17:33:39 -0500

    Update IOMainLayoutBackground.tsx

--- client/src/components/innerOrbit/layout/IOmainLayout/IOMainLayoutBackground.tsx
+++ client/src/components/innerOrbit/layout/IOmainLayout/IOMainLayoutBackground.tsx
@@ -1,7 +1,7 @@
 // File: client/src/components/innerOrbit/layout/IOmainLayout/IOMainLayoutBackground.tsx
 
 import React from 'react';
-import styles from '../../../assets/css/innerOrbit/layout/IOMainLayoutBackground.module.css';
+import styles from '../../../../../assets/css/innerOrbit/layout/IOMainLayoutBackground.module.css';
 
 const IOMainLayoutBackground: React.FC = () => {
   return <div className={styles['main-background']} />;
