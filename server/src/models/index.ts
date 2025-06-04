b6adc4eaaec545ee74bc6b929e7cc0543641ad18
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-04 17:19:27 -0500

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,8 +1,8 @@
 // File: server/src/models/index.ts
 
 import User from './User.js';
-import MoodEntry from './Tracker.js';
-import JournalEntry from './Journal.js';
+import MoodEntry from './innerOrbit/Tracker.js';
+import JournalEntry from './innerOrbit/Journal.js';
 
 export {
   User,
