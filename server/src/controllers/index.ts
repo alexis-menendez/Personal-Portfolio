feed356d181294dd27d9a2eb960a6ce7fd2f355e
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-04 17:17:58 -0500

    Update index.ts

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -4,5 +4,3 @@ export * from './userController.js';
 export * from './innerOrbit/trackerController.js';
 export * from './innerOrbit/journalController.js';  
 export * from './innerOrbit/libraryController.js';
-
-// Add more exports here as needed
