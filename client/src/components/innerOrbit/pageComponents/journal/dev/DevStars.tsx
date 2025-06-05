82a56d365fb4ec7c6bc953a596fdb9c3d5450442
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-05 14:52:39 -0500

    Update DevStars.tsx

--- client/src/components/innerOrbit/pageComponents/journal/dev/DevStars.tsx
+++ client/src/components/innerOrbit/pageComponents/journal/dev/DevStars.tsx
@@ -1,7 +1,7 @@
 // File: client/src/components/innerOrbit/pageComponents/journal/dev/DevStars.tsx
 
 import React from 'react';
-import ShootingStar from '../../../../common/ShootingStar';
+import ShootingStar from '../../../common/ShootingStar';
 
 const DevelopStars: React.FC = () => {
   return (
