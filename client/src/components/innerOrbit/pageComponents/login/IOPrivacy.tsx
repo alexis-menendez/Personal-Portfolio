b33e3cef95e38e9dc600c6bfdec49416b09f48c4
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-05 13:36:23 -0500

    Update IOPrivacy.tsx

--- client/src/components/innerOrbit/pageComponents/login/IOPrivacy.tsx
+++ client/src/components/innerOrbit/pageComponents/login/IOPrivacy.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // Components
-import TransparentStars from '../common/TransparentStars';
-import ShootingStar from '../common/ShootingStar';
+import TransparentStars from '../../common/TransparentStars';
+import ShootingStar from '../../common/ShootingStar';
 
 // Styles
 import styles from '../../../../../assets/css/innerOrbit/pageStyles/login/IOPrivacyPolicy.module.css';
 import buttonStyles from '../../../../../assets/css/innerOrbit/common/IOButton.module.css';
 import logoStyles from '../../../../../assets/css/innerOrbit/common/IOLogo.module.css';
+
 
 const Privacy = () => {
   const navigate = useNavigate();
