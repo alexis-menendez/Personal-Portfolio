b656547cf14a102076f4d098d117a0e475e35030
Alex Menendez <alexis.menendez@austincc.edu>
2025-06-05 15:13:53 -0500

    Update IOTerms.tsx

--- client/src/components/innerOrbit/pageComponents/login/IOTerms.tsx
+++ client/src/components/innerOrbit/pageComponents/login/IOTerms.tsx
@@ -8,9 +8,9 @@ import TransparentStars from '../../common/TransparentStars';
 import ShootingStar from '../../common/ShootingStar';
 
 // Styles
-import styles from '../../../../../assets/css/innerOrbit/pageStyles/login/IOTerms.module.css';
-import buttonStyles from '../../../../../assets/css/innerOrbit/common/IOButton.module.css';
-import logoStyles from '../../../../../assets/css/innerOrbit/common/IOLogo.module.css';
+import styles from '../../../../assets/css/innerOrbit/pageStyles/login/IOTerms.module.css';
+import buttonStyles from '../../../../assets/css/innerOrbit/common/IOButton.module.css';
+import logoStyles from '../../../../assets/css/innerOrbit/common/IOLogo.module.css';
 
 
 
