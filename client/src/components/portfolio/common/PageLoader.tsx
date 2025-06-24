c96beb3318301dc41d923ae830473fe0b4afda98
Alexis Menendez <alexis.menendez@austincc.edu>
2025-06-23 23:28:37 -0500

    changed location of lottie file

--- client/src/components/portfolio/common/PageLoader.tsx
+++ client/src/components/portfolio/common/PageLoader.tsx
@@ -1,22 +1,14 @@
 // File: client/src/components/portfolio/common/PageLoader.tsx
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Lottie from 'lottie-react';
+import animationData from '../../../assets/animations/portfolio/NightLoadingAnimation.json'; 
 import styles from '../../../assets/css/portfolio/common/PageLoader.module.css';
 
 const PageLoader: React.FC = () => {
-  const [animationData, setAnimationData] = useState(null);
-
-  useEffect(() => {
-    fetch('/assets/portfolio/animations/NightLoadingAnimation.json')
-      .then((res) => res.json())
-      .then(setAnimationData)
-      .catch((err) => console.error('Failed to load animation:', err));
-  }, []);
-
   return (
     <div className={styles.loaderOverlay}>
-      {animationData && <Lottie animationData={animationData} loop autoplay />}
+      <Lottie animationData={animationData} loop autoplay />
     </div>
   );
 };
